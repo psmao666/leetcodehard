efe23abf92e506b4298982157646ffb9a1bd2c73
psmao666 <113750853+psmao666@users.noreply.github.com>
2022-10-20 12:12:56 +1100

    Update and rename divide_two_integers to divide_two_integers.py

--- divide_two_integers.py
+++ divide_two_integers.py
@@ -1,3 +1,4 @@
+''
 Problem link : https://leetcode.com/problems/divide-two-integers/
 
 Solution:
@@ -7,6 +8,7 @@ Knowing that any number can be represented in binary form, then we find the bigg
 Now we just need to check the bits in range of g.
 
 The time complexity is O(logN)
+''
 
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
